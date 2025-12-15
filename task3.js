908767a7012afda4e6f784fc7af1c403a6540e34
developer-rolando <rolando.garcia.sanchez1@gmail.com>
2025-12-15 14:14:10 -0600

    Imprimiendo FizzBuzz si los numeros son multiplos de 3 y 5, Fizz si solo de 3, Buzz si solo de 5

--- task3.js
+++ task3.js
@@ -3,3 +3,9 @@
 for (let i = 0; i < 1; i++) {
     console.log("This is Task Three!");
   };
+
+     //Imprimiendo numeros del 1 - 105 e imprimiento FizzBuzz si es multiplo de 3 y 5, 
+     // Fizz si solo de 3, Buzz si solo de 5
+for (let index = 1; index <= 105; index++) {
+    index % 3 == 0 && index % 5 == 0? console.log("FizzBuzz") : index % 3 == 0 ? console.log("Fizz") : index % 5 == 0 ? console.log("Buzz") : console.log(index);
+}
