b415afc1d1a165ec023f2fca964fc7bbea56c04a
developer-rolando <rolando.garcia.sanchez1@gmail.com>
2025-12-15 13:48:56 -0600

    codigo que muestra numeros del 1-105 en archivo task1.js

--- task1.js
+++ task1.js
@@ -1,3 +1,9 @@
 // Refer to Task 1 in your Instructions to complete this task
 const prompt = require("prompt-sync")();
 const name = prompt("Ingresa tu nombre");
+
+//Imprimiendo numeros del 1 - 104
+for (let index = 1; index <= 105; index++) {
+    console.log(index);
+    
+}
